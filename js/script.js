75ead35e94bb4f6157066a6d2ca5cdea4df753f2
belén <belusolocosasimportantes@gmail.com>
2024-08-18 16:05:41 -0300

    fetch - mostrar estudiantes

--- js/script.js
+++ js/script.js
@@ -19,3 +19,17 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then(response => {
+    if(!response.ok){
+      throw new Error("Error al cargar los datos")
+    }
+    return response.json();
+  })
+  .then(data => {
+    showData(data.students);
+  })
+
+  .catch(error => {
+    console.error("Hubo un problema con la petición Fetch realizada:", error);
+  });
